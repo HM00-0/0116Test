4f248ebc57fb4a103925e7553b3eb25a330ac41e
HM00-0 <hmryu@outlook.com>
2025-02-04 16:14:15 +0900

    change 3

--- OllamaWorld/OllamaWorld/OllamaWorld/obj/Debug/net8.0/OllamaWorld.AssemblyInfo.cs
+++ OllamaWorld/OllamaWorld/OllamaWorld/obj/Debug/net8.0/OllamaWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OllamaWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45701ee851c76ac1789f36a1093edefc5da1e495")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d72c4e0dae9430d82ad529fd4c51e2242904b27c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OllamaWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OllamaWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
